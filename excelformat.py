1c1e6d874b402abc0fb88a896b05a7c88bf88a01
adamhammond22 <abhammond22@gmail.com>
2023-02-18 19:53:34 -0800

    correct and incorrect digs implemented, unfinished
    must go thru setup to determine if leave is incorrect or correct dig

--- excelformat.py
+++ excelformat.py
@@ -12,7 +12,20 @@ root.withdraw() #Hide root window
 #File->Info->CheckForIssues->InspectDocument(for headers/footers)->Remove header and footer
 #I've done this once and it does not impact the program
 
+#Formats dig column at all row indices in dataframe with label
+#Also removes all consecutive digs before the first and last dig
+#Returns indices to drop
+def formatDigs(df, indices, label):
+	#Grab first row off of indices and set
+	firstrow = indices.pop(0)
+	df.iat[firstrow,1] = label
+	
+	if (len(indices) > 0): # if theres another element		
+		#Grab last row off of indices and set
+		lastrow = indices.pop(len(indices)-1)
+		df.iat[lastrow,1] = label
 
+	return(indices)
 
 #to make non-hardcoded just swap which one is commented
 filepath = "C:/Users/Adam/Desktop/Aster/A005_EDS_tab_new.xlsx" #HARDCODED FILEPATH
@@ -55,8 +68,11 @@ BehaviorDF = pd.DataFrame(columns=["Time", "Behavior", "Behavior type", "Trial"]
 # ========== Iterate Through Raw File ========== #
 
 trial = 0 #Counter to track Trial Number
+dig_row_indices = [] #List of indices from most recent digs
+drop_indices = [] # List of all indices to drop
 
 #Iterate over all rows in Raw Dataframe
+digging = False
 for index, row in RawDF.iterrows():
 
 	#Write according text based on raw behavior character(s, t, v, etc...)
@@ -72,9 +88,28 @@ for index, row in RawDF.iterrows():
 			behavior = "ApproachRight"
 		case "v":
 			behavior = "Leave"
+			#If we were just digging, we need to check the setup to determine
+			if digging:
+				# TrialIndex = SetupDF.get_loc("Trial")
+				# L_TextureIndex = SetupDF.get_loc("L_Texture")
+				# CorrTexture = SetupDF.iat[0, TrialIndex]
+				# CorrTexture = SetupDF.iat[trial, TrialIndex]
+				# SetupDF.get_loc
+				drop_indices.extend(formatDigs(BehaviorDF, dig_row_indices, "IncorrectDig"))
+				digging = False
+				dig_row_indices = []
 		case "e":
 			behavior = "Eat"
+			#If we were just digging, label all prev digs as incorrect
+			if digging:
+				drop_indices.extend(formatDigs(BehaviorDF, dig_row_indices, "CorrectDig"))
+				digging = False
+				dig_row_indices = []
 		case "d":
+			#Digs are complicated, we cannot label as correct or incorrect until
+			#we iterate to the next row, so we simply write a placeholder "Dig" and track index
+			digging = True
+			dig_row_indices.append(index);
 			behavior = "Dig"
 		case _:
 			#Default case: just print the undefined behavior and warn the user
@@ -84,7 +119,8 @@ for index, row in RawDF.iterrows():
 	#Append new row of collected data to Behavior Dataframe
 	BehaviorDF.loc[len(BehaviorDF)] = [str(row["Time"]), behavior, row["Behavior type"], str(trial)]
 
-
+print(drop_indices)
+BehaviorDF = BehaviorDF.drop(labels=drop_indices, axis=0)
 
 #Highlights a row based on the behavior of that row
 def highlight(s):
@@ -95,7 +131,9 @@ def highlight(s):
 			return ['color: #C65911'] * len(s)
 		case "ApproachLeft":
 			return ['color: #C65911'] * len(s)
-		case "Dig":
+		case "CorrectDig":
+			return ['color: ##548235'] * len(s)
+		case "IncorrectDig":
 			return ['color: #FF0000'] * len(s)
 		case _:
 			return ['color: black'] * len(s)
